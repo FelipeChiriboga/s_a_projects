af024ed2e6162f8a29c043972fcd073d94fabf2a
FelipeChiriboga <felipea.chiriboga@hotmail.com>
2019-08-16 15:34:46 +0200

    update

--- Week_05/Items_csv.pl
+++
@@ -1,9 +0,0 @@
-item,height,width,depth
-blue cylinder,14.9,4.5,4.5
-green cube,5.0,5.0,4.5
-wooden ball,7.0,7.0,7.0
-spiky ball,6.0,6.0,6.0
-minion,4.0,2.0,2.0
-red cylinder,4.0,4.0,4.0
-penguin,10.0,3.0,3.0
-disc,10.0,10.0,1.5
